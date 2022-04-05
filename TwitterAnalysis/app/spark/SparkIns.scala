64f29c18b1c6e3b94cc199073df8ade40ceaac3e
abbottqwq <li.zhuohui@northeastern.edu>
2022-04-04 22:03:50 -0400

    feat: init spark

---
+++ TwitterAnalysis/app/spark/SparkIns.scala
@@ -0,0 +1,19 @@
+package spark
+
+import org.apache.spark.sql.SparkSession
+import play.api.Configuration
+
+import javax.inject._
+
+import utils.MyConfigLoader._
+
+@Singleton
+case class SparkIns @Inject()(config: Configuration) {
+	val spark = SparkSession.builder()
+		.appName(config.getStringOption("SPARK_APP_NAME").getOrElse("finalproject"))
+		.master(config.getStringOption("SPARK_MASTER").getOrElse("local[*]"))
+		.getOrCreate()
+
+	def getSpark = spark
+}
+
