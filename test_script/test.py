24f69c4d091cc7f41ec721129e316943bf4f3385
abbottqwq <li.zhuohui@northeastern.edu>
2022-04-10 06:13:28 -0400

    fix: fix an error in test.py

--- test_script/test.py
+++ test_script/test.py
@@ -14,7 +14,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         actions = ["testconnect", "testspark", "testdatabase"]
         for action in actions:
-            self.assertIn("Success", req(action))  # add assertion here
+            self.assertNotIn("Error", req(action))  # add assertion here
 
 
 if __name__ == '__main__':
