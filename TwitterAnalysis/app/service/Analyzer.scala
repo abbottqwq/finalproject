30df9a76b554dbb09111ffe2e3ffffbd4e4ff245
songyang <songyang719@163.com>
2022-04-19 19:29:12 -0400

    feat: update preprocessing

--- TwitterAnalysis/app/service/Analyzer.scala
+++ TwitterAnalysis/app/service/Analyzer.scala
@@ -1,58 +1,31 @@
 package service
 
-import com.johnsnowlabs.nlp.DocumentAssembler
-import com.johnsnowlabs.nlp.annotators.Tokenizer
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, lower, regexp_replace, split, trim}
-import spark.SparkIns
+import _root_.spark.SparkIns
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import javax.inject.{Inject, Singleton}
 
-object Analyzer extends App {
-	val spark: SparkSession = SparkSession
-		.builder()
-		.appName("TweetAnalyzer")
-		.master("local[*]")
-		.getOrCreate()
-	spark.sparkContext.setLogLevel("ERROR")
+@Singleton
+case class Analyzer @Inject()(sparkIns: SparkIns) extends AnalyzerBase {
 
-	val df = spark.read.option("delimiter", ",").option("header", "true").csv("resources/sample.csv")
-	print(df.select("text").head().toString())
-
-
-	// clean data, remove first word
-	val df_without_company = df.withColumn("text", split(col("text"), " ", 2)(1))
-
-	// clean data, trim + toLowercase + remove anything except Alphanumeric and Punctuations
-	val clean_df = df_without_company.withColumn("text",
-		regexp_replace(trim(lower(col("text"))), "[^ 'a-zA-Z0-9,.?!]", ""))
-	print(clean_df.select("text").head().toString())
-
-	// preprocessing
-	// split each document into tokens
-	val documentAssembler = new DocumentAssembler().setInputCol("text").setOutputCol("document").setCleanupMode("shrink")
-
-	val tokenizer = new Tokenizer().setInputCols("document").setOutputCol("token")
-
+  def run(sparkIns: SparkIns): DataFrame = {
+    val spark: SparkSession = sparkIns.spark
+    val path: String = getClass.getResource("/twcs.csv").getPath
+    val df: DataFrame = spark.read.option("delimiter", ",").option("header", "true").csv(path)
+    super.preprocessing(df)
+  }
 }
 
-@Singleton
-class Analyzer @Inject()(sparkIns: SparkIns) {
-	def testRun = {
-		val spark = sparkIns.spark
-		val df = spark.read.option("delimiter", ",").option("header", "true").csv("resources/sample.csv")
-		print(df.select("text").head().toString())
-		// clean data, remove first word
-		val df_without_company = df.withColumn("text", split(col("text"), " ", 2)(1))
-		// clean data, trim + toLowercase + remove anything except Alphanumeric and Punctuations
-		val clean_df = df_without_company.withColumn("text",
-			regexp_replace(trim(lower(col("text"))), "[^ 'a-zA-Z0-9,.?!]", ""))
-		print(clean_df.select("text").head().toString())
+object Analyzer extends AnalyzerBase with App {
+  val spark: SparkSession = SparkSession
+    .builder()
+    .appName("TwitterAnalysis")
+    .master("local[*]")
+    .getOrCreate()
 
-		// preprocessing
-		// split each document into tokens
-		val documentAssembler = new DocumentAssembler().setInputCol("text").setOutputCol("document").setCleanupMode("shrink")
-
-		val tokenizer = new Tokenizer().setInputCols("document").setOutputCol("token")
-	}
+  spark.sparkContext.setLogLevel("ERROR")
+  val path: String = getClass.getResource("/twcs.csv").getPath
+  val df = spark.read.option("delimiter", ",").option("header", "true").csv(path)
+  val result = super.preprocessing(df)
+  result.select("new_text").show(20, truncate = false)
 }
