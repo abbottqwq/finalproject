9ce1df9aa6e03d10eed7bbd317b753fb1a61c795
abbottqwq <71251245+abbottqwq@users.noreply.github.com>
2022-04-20 18:32:18 -0400

    Revert "Preprocessing"

--- TwitterAnalysis/app/controllers/TestController.scala
+++ TwitterAnalysis/app/controllers/TestController.scala
@@ -3,72 +3,67 @@ package controllers
 import play.api._
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.mvc._
-import service.Analyzer
 import slick.jdbc.JdbcProfile
 import spark.SparkIns
-import utils.implicits.MyConfigLoader._
 import utils.implicits.MyToJson._
+import utils.implicits.MyConfigLoader._
 
 import javax.inject._
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 import scala.util._
+import utils.implicits.MyConfigLoader._
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class TestController @Inject()(cc: ControllerComponents, config: Configuration, sparkIns: SparkIns, dbConfigProvider: DatabaseConfigProvider, analyzer: Analyzer) extends AbstractController(cc) {
+class TestController @Inject()(cc: ControllerComponents, config: Configuration, sparkIns: SparkIns, dbConfigProvider: DatabaseConfigProvider) extends AbstractController(cc) {
 
-  def test(action: String) = Action {
-    implicit request: Request[AnyContent] => {
-      config.getStringOption("ENABLE_TEST_URI").getOrElse("0") match {
-        case "0" => Forbidden("only for dev mode")
-        case "1" =>
-          action match {
-            case "testconnect" => Ok(("Success" -> "1").toJson)
-            case "testspark" => testSpark(sparkIns)
-            case "testdatabase" => testDatabase(dbConfigProvider)
-            case "closespark" =>
-              Try(sparkIns.stopSpark) match {
-                case Success(_) => Ok(("Success" -> "1").toJson)
-                case Failure(_) => Ok(("Success" -> "0").toJson)
-              }
-            case "testpreprocess" => testPreProcess(sparkIns)
-            case _ => NotFound("No Such Test")
-          }
-      }
-    }
-  }
+	def test(action: String) = Action {
+		implicit request: Request[AnyContent] => {
+			config.getStringOption("ENABLE_TEST_URI").getOrElse("0") match {
+				case "0" => Forbidden("only for dev mode")
+				case "1" =>
+					action match {
+						case "testconnect" => Ok(("Success" -> "1").toJson)
+						case "testspark" => testSpark(sparkIns)
+						case "testdatabase" => testDatabase(dbConfigProvider)
+						case "closespark" =>
+							Try(sparkIns.stopSpark) match {
+								case Success(_) => Ok(("Success" -> "1").toJson)
+								case Failure(_) => Ok(("Success" -> "0").toJson)
+							}
+						case _ => NotFound("No Such Test")
+					}
+			}
+		}
+	}
 
-  def testSpark(sparkIns: SparkIns): Result = {
-    Ok((Try(sparkIns.spark.sessionState) match {
-      case Success(_) =>
-        Try(sparkIns.spark.sparkContext.appName) match {
-          case Success(name) if name == config.getStringOption("SPARK_APP_NAME").getOrElse("finalproject") => Map("Success" -> "1", "AppName" -> name)
-          case Success(name) => Map("Success" -> "1", "Error" -> "AppName error", "AppName" -> name)
-          case Failure(f) => Map("Success" -> "0", "Error" -> "Spark connection fail", "Reason" -> f.toString)
-        }
-      case Failure(f) => Map("Success" -> "0", "Error" -> "Spark connection fail", "Reason" -> f.toString)
-    }).toJson)
+	def testSpark(sparkIns: SparkIns): Result = {
+		Ok((Try(sparkIns.spark.sessionState) match {
+			case Success(_) =>
+				Try(sparkIns.spark.sparkContext.appName) match {
+					case Success(name) if name == config.getStringOption("SPARK_APP_NAME").getOrElse("finalproject") => Map("Success" -> "1", "AppName" -> name)
+					case Success(name) => Map("Success" -> "1", "Error" -> "AppName error", "AppName" -> name)
+					case Failure(f) => Map("Success" -> "0", "Error" -> "Spark connection fail", "Reason" -> f.toString)
+				}
+			case Failure(f) => Map("Success" -> "0", "Error" -> "Spark connection fail", "Reason" -> f.toString)
+		}).toJson)
 
-  }
+	}
 
-  def testDatabase(dbConfigProvider: DatabaseConfigProvider): Result = {
-    val dbConfig = dbConfigProvider.get[JdbcProfile]
-    import dbConfig._
-    import profile.api._
-    Ok((Try(Await.result(db.run(sql"SELECT * FROM pg_catalog.pg_tables;".as[String]), Duration.Inf)) match {
-      case Success(_) => Map("Success" -> "1")
-      case Failure(f) => Map("Success" -> "0", "Error" -> "database connection fail", "Reason" -> f.toString)
-    }).toJson)
-  }
-
-  def testPreProcess(sparkIns: SparkIns): Result = {
-    Ok((Try(analyzer.run(sparkIns)) match {
-      case Success(_) => Map("Success" -> "1")
-      case Failure(f) => Map("Success" -> "0", "Error" -> "preprocess test fail", "Reason" -> f.toString)
-    }).toJson)
-  }
+	def testDatabase(dbConfigProvider: DatabaseConfigProvider): Result = {
+		val dbConfig = dbConfigProvider.get[JdbcProfile]
+		import dbConfig._
+		import profile.api._
+		Ok((Try(Await.result(db.run(sql"SELECT * FROM pg_catalog.pg_tables;".as[String]), Duration.Inf)) match {
+			case Success(_) => Map("Success" -> "1")
+			case Failure(f) => Map("Success" -> "0", "Error" -> "database connection fail", "Reason" -> f.toString)
+		}).toJson)
+	}
 }
