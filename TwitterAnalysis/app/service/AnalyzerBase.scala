9afaaeca91a85f4e026e7bc152f32a315b51b53c
abbottqwq <li.zhuohui@northeastern.edu>
2022-04-23 05:27:39 -0400

    chore: fix

---
+++ TwitterAnalysis/app/service/AnalyzerBase.scala
@@ -0,0 +1,74 @@
+package service
+
+import com.johnsnowlabs.nlp.annotators._
+import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetector
+import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, regexp_replace, split, trim}
+
+class AnalyzerBase {
+
+	def preprocessing(df: DataFrame): DataFrame = {
+		// clean data, keep users' responses, remove companies' responses and irregular responses
+		val df_true = df.filter("inbound=TRUE")
+			.filter(col("text").startsWith("@"))
+
+		// clean data, remove first word, replace author_id with company_name
+		val split_array = split(col("text"), " ", 2)
+		val df_with_company = df_true.withColumn("author_id", split_array(0)).withColumn("text", split_array(1))
+
+		// clean data, trim + remove urls
+		val cleaned_df = df_with_company.withColumn("text",
+			regexp_replace(trim(col("text")), "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", ""))
+
+		cleaned_df.select("text").show(10, truncate = false)
+
+		// preprocessing
+		val documentAssembler = new DocumentAssembler()
+			.setInputCol("text")
+			.setOutputCol("document")
+
+		val sentenceDetector = new SentenceDetector()
+			.setInputCols(Array("document"))
+			.setOutputCol("sentence")
+
+		val tokenizer = new Tokenizer()
+			.setInputCols(Array("sentence"))
+			.setOutputCol("token")
+
+		val normalizer = new Normalizer()
+			.setInputCols("token")
+			.setOutputCol("normalized")
+			.setLowercase(true)
+			.setCleanupPatterns(Array("""[^A-Za-z ]"""))
+
+		val stopWords = new StopWordsCleaner()
+			.setInputCols("normalized")
+			.setOutputCol("cleanTokens")
+			.setCaseSensitive(false)
+
+		val lemma = new Lemmatizer()
+			.setInputCols("cleanTokens")
+			.setOutputCol("lemma")
+			.setDictionary("dependencies/lemma.txt", "->", "\t")
+
+		val finisher = new Finisher()
+			.setInputCols("lemma")
+			.setOutputCols("new_text")
+			.setCleanAnnotations(false)
+
+		val pipeline = new Pipeline().setStages(Array(
+			documentAssembler,
+			sentenceDetector,
+			tokenizer,
+			normalizer,
+			stopWords,
+			lemma,
+			finisher
+		))
+
+		pipeline.fit(cleaned_df).transform(cleaned_df)
+	}
+
+}
