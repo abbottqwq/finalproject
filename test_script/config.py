8d287548132a6a3392b6f3cce45af753a0db3c83
abbottqwq <li.zhuohui@northeastern.edu>
2022-04-25 06:54:08 -0400

    fix: port for test

--- test_script/config.py
+++ test_script/config.py
@@ -1,5 +1,5 @@
 import os
 HOST = os.environ.get("HOST", "localhost")
-PORT = os.environ.get('PORT', "9999")
+PORT = os.environ.get('PORT', "9000")
 PREFIX = os.environ.get('PREFIX', "api")
 URL = f"http://{HOST}:{PORT}/{PREFIX}/test/"
