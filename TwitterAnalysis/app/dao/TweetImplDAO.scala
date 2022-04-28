56e97621a20654a71b8dd2f76fae4252922ad7f7
Yijun <yonghengyixin@gamil.com>
2022-04-27 23:05:13 -0400

    use spark write sql

--- TwitterAnalysis/app/dao/TweetImplDAO.scala
+++ TwitterAnalysis/app/dao/TweetImplDAO.scala
@@ -1,5 +1,6 @@
 package dao
 
+import org.antlr.v4.runtime.atn.SemanticContext.AND
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SaveMode}
 import spark.SparkIns
@@ -43,13 +44,6 @@ class TweetImplDAO @Inject()(sparkIns: SparkIns) extends DAO {
 	 * get keywords from a time period sort by frequency
 	 */
 	def readByTime(start: String, end: String): DataFrame = {
-//		sparkIns.spark.sql(s"SELECT tt.tweets, " +
-//									s"COUNT ( tt.tweets ) AS freq FROM t_customer_support tcs LEFT JOIN t_tweets tt ON tcs.tweet_id = tt.base_id " +
-//									s"WHERE to_date( tcs.created_at, 'E MMM dd HH:mm:ss Z yyyy' ) >= '${start}' " +
-//									s"and to_date( tcs.created_at, 'E MMM dd HH:mm:ss Z yyyy' ) < '${end}' GROUP BY " +
-//									s"tt.tweets " +
-//									s"ORDER BY freq desc")
-
 		val df_tweet = sparkIns.spark.table("t_tweets")
 		val df_cs = sparkIns.spark.table("t_customer_support")
 		df_tweet
@@ -65,23 +59,28 @@ class TweetImplDAO @Inject()(sparkIns: SparkIns) extends DAO {
 	 * get keywords from specific company sort and time period sort by frequency
 	 */
 	def readByCompanyAndTime(name: String, start: String, end: String): DataFrame = {
-		sparkIns.spark.sql(s"SELECT tt.tweets, " +
-							s"COUNT ( tt.tweets ) AS freq " +
-							s"FROM t_customer_support tcs LEFT JOIN t_tweets tt ON tcs.tweet_id = tt.base_id " +
-							s"WHERE tcs.author_id = '${name}' AND " +
-							s"to_date( tcs.created_at, 'E MMM dd HH:mm:ss Z yyyy' )  >= '${start}' " +
-							s"and to_date( tcs.created_at, 'E MMM dd HH:mm:ss Z yyyy' ) < '${end}' GROUP BY " +
-							s"tt.tweets " +
-							s"ORDER BY freq desc")
+		val df_tweet = sparkIns.spark.table("t_tweets")
+		val df_cs = sparkIns.spark.table("t_customer_support")
+		df_tweet
+			.join(df_cs, df_tweet.col("base_id") === df_cs.col("tweet_id"), "left_outer")
+			.where(df_cs("author_id") === name && to_date(col("created_at"), "E MMM dd HH:mm:ss Z yyyy").between(start, end))
+			.groupBy("tweets")
+			.agg(count("*") as "freq")
+			.orderBy(col("freq").desc)
+			.toDF()
 	}
 
 	/**
 	* get Top 20 Company
 		*/
 	def getTop20Company: DataFrame = {
-		sparkIns.loadRead()
-			.option("query", s"SELECT tcs.author_id, COUNT ( tcs.author_id ) AS freq FROM t_customer_support tcs " +
-				s"GROUP BY tcs.author_id ORDER BY freq desc Limit 20")
-			.load()
+		val df_tweet = sparkIns.spark.table("t_tweets")
+		val df_cs = sparkIns.spark.table("t_customer_support")
+		df_tweet
+			.join(df_cs, df_tweet.col("base_id") === df_cs.col("tweet_id"), "left_outer")
+			.groupBy("author_id")
+			.agg(count("*") as "freq")
+			.orderBy(col("freq").desc)
+			.toDF()
 	}
 }
